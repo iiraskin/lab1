a9fc01e7f5130840521f5296f5ad3b528b3bf247
iiraskin <iraskin@mail.ru>
2017-04-11 01:37:01 +0300

    Right codstile

--- mkdir/main.c
+++ mkdir/main.c
@@ -9,56 +9,55 @@
 #include <sys/stat.h>
 
 
-void Create_directory( char* path, char option ){
+void Create_directory (char* path, char option) {
     int i = 0;
 
     /*Check if the path exist*/
-    for(; path[i] != 0; i++ ){
+    for (i; path[i] != 0; i++) {
         /*Check each directory in the path*/
-        if( path[i] == '/' ){
+        if (path[i] == '/') {
             path[i] = 0;
-            DIR* dir = opendir( path );
-            if ( !dir && strcmp( path, "\0" ) )
-            {
-                if( option ){
+            DIR* dir = opendir (path);
+            if (!dir && strcmp (path, "\0")) {
+                if (option) {
                     /*If a directory in the path doesn't exist -
                     it should be created*/
-                    mkdir( path, 0744 );
-                }else{
+                    mkdir (path, 0744);
+                } else {
                     /*If the path doesn't exist - it's an error*/
-                    printf( "Incorrect path: %s \n", path );
-                    error( 1, errno, " " );
+                    printf ("Incorrect path: %s \n", path);
+                    error (1, errno, " ");
                 }
             }
-            closedir( dir );
+            closedir (dir);
             path[i] = '/';
         }
     }
 
     /*Create the directory*/
-    int r = mkdir( path, 0744 );
-    if( r ){
-        printf( "This directory already exist. \n" );
+    int r = mkdir (path, 0744);
+    if (r) {
+        printf ("This directory already exist. \n");
     }
 }
 
-int main( int argc, char* argv[] )
+int main (int argc, char* argv[])
 {
     char date;
     char option = 0;
 
     /*Determine which option should be used*/
-	while( ( date = getopt( argc, argv, "-p" ) ) != -1 ){
-        if( date == 'p' ){
+    while ((date = getopt(argc, argv, "-p")) != -1) {
+        if (date == 'p') {
             option = 'p';
         }
-	}
+    }
 
-	/*For each element of argv call chosen function*/
+    /*For each element of argv call chosen function*/
     int i = 1;
-    for(; i < argc; ++i ){
-        if( strcmp( argv[i], "-p" ) ){
-            Create_directory( argv[i], option );
+    for (i; i < argc; ++i) {
+        if (strcmp (argv[i], "-p")) {
+            Create_directory (argv[i], option);
         }
     }
 
